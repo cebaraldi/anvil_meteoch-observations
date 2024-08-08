d22f5eb066808278b4cff70ef5a94f5a0e6bb0c8
cebaraldi@gmail.com <cebaraldi@gmail.com>
2024-08-08 08:48:58 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,16 +11,19 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.ws = []
-    self.ws.append(anvil.server.call('dl'))
-    print(self.ws)
-    #self.drop_down_ClimateRegion.items = sorted(set(self.ws['ClimateRegion']))
+    self.ws = anvil.server.call('dl')
+    self.drop_down_ClimateRegion.items = sorted(set(self.ws['ClimateRegion']))
     self.label_Station.visible = False
     self.drop_down_Station.visible = False
 
   def drop_down_ClimateRegion_change(self, **event_args):
     """This method is called when an item is selected"""
     cr = self.drop_down_ClimateRegion.selected_value
+    print(cr)
+    print(self.ws)
+    newl = [r for r in self.ws if (r['ClimateRegion'] == cr)]
+    #newl = [d for d in dictn if d['Age'] > aget]
+    print(newl)
     self.label_Station.visible = True
     self.drop_down_Station.visible = True
     #self.drop_down_Station.items = self.app.state.xyz
