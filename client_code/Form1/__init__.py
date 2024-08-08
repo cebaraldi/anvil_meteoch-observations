2dcb466499b4fab2f10b325d9449336fcedce0f6
cebaraldi@gmail.com <cebaraldi@gmail.com>
2024-08-08 05:29:43 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,19 +15,16 @@ class Form1(Form1Template):
     self.drop_down_ClimateRegion.items = sorted(set(ws['ClimateRegion']))
     self.label_Station.visible = False
     self.drop_down_Station.visible = False
-    #self.drop_down_Station.items = ['0', '1', '2', '3']
 
   def drop_down_ClimateRegion_change(self, **event_args):
     """This method is called when an item is selected"""
-    #self.drop_down_ClimateRegion.visible = True
-    #stations = anvil.server.call('get_ws', self.drop_down_ClimateRegion.selected_value)
-    #self.wsDropdown.items = stations
+    self.label_Station.visible = True
+    self.drop_down_Station.visible = True
+    self.drop_down_Station.items = ['0', '1', '2', '3']    
 
   def drop_down_Station_change(self, **event_args):
     """This method is called when an item is selected"""
-    self.label_Station.visible = True
-    self.drop_down_Station.visible = True
-    self.drop_down_Station.items = ['0', '1', '2', '3']
+    pass
 
 
 
